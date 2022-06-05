409bf42698ef6369341d55cf8f2a87da9020283d
ShameenShetty <57455717+ShameenShetty@users.noreply.github.com>
2022-06-04 19:04:26 -0500

    created scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(Scaffold(
+      appBar: AppBar(
+        title: const Text('First Project'),
+      ),
+      body: Center(
+        child: Column(
+          children: const [
+            Text('First Text'),
+            Text('Second Text'),
+            Text('Third Text'),
+          ],
+        ),
+      ),
+    ));
