c742e321e0e541da99a089f0065f918fabf79db2
ShameenShetty <57455717+ShameenShetty@users.noreply.github.com>
2022-06-04 19:19:04 -0500

    wrapped the Scaffold in the MaterialApp since I get an error otherwise "No MediaQuery widget ancestor found"
    added FloatingActionButton
    some minor changes to the Scaffold as well

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Scaffold(
-      appBar: AppBar(
-        title: const Text('First Project'),
-      ),
-      body: Center(
-        child: Column(
-          children: const [
-            Text('First Text'),
-            Text('Second Text'),
-            Text('Third Text'),
-          ],
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('First Project'),
+          centerTitle: true,
+          backgroundColor: Colors.purple[400],
+        ),
+        body: Center(
+          child: Column(
+            children: const [
+              Text('First Text'),
+              Text('Second Text'),
+              Text('Third Text'),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Text('Open'),
+          onPressed: () {},
         ),
       ),
     ));
