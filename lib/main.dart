6a061849c386c6ac6cc5e8c5c8ef720de50f1f7f
Shameen Shetty <shettyshameen1@gmail.com>
2022-06-30 14:49:35 -0500

    added stateless Home widget to the main func,
    added const modifiers wherever needed

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'First Project',
-            style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Arima'),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.purple[400],
-        ),
-        body: Center(
-          child: Column(
-            children: const [
-              Text('First Text'),
-              Text('Second Text'),
-              Text('Third Text'),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Text('Open'),
-          onPressed: () {},
-        ),
-      ),
+void main(List<String> args) => runApp(const MaterialApp(
+      home: Home(),
     ));
 
 class Home extends StatelessWidget {
@@ -31,6 +9,28 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'First Project',
+          style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Arima'),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.purple[400],
+      ),
+      body: Center(
+        child: Column(
+          children: const [
+            Text('First Text'),
+            Text('Second Text'),
+            Text('Third Text'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Text('Open'),
+        onPressed: () {},
+      ),
+    );
   }
 }
